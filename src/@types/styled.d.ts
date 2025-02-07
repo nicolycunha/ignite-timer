c5fc20a07cb38774501179b92623b4009fb0c11d
Nicoly Cunha <nicolyoliveiracunha.2001@gmail.com>
2025-02-07 20:37:49 -0300

    feat: create application structure

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -1,9 +1,10 @@
 import 'styled-components'
-import {defaultTheme} from '../styles/themes/default'
+import { defaultTheme } from '../styles/themes/default'
 
 type ThemeType = typeof defaultTheme
 
 declare module 'styled-components' {
-    export interface DefaultTheme extends ThemeType {}
+    export interface DefaultTheme extends ThemeType {
+        _? : never
+    } 
 }
-
